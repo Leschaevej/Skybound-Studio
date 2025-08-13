60d3ace9f4470a0bc608ed090acdbc8364fde174
Leschaevej <leschaeve.jimmy@gmail.com>
2025-08-13 18:10:00 +0200

    0.2.3.2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/next";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import "./globals.scss";
 import { robotoCondensed } from "./font";
 
-export const metadataBase = new URL("https://skyboundstudio.fr");
+export const metadataBase = new URL("https://skybound-studio.vercel.app/");
 
 export const metadata: Metadata = {
     title: {
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
         "design web",
         "Skybound Studio",
     ],
-    authors: [{ name: "Skybound Studio", url: "https://skyboundstudio.fr" }],
+    authors: [{ name: "Skybound Studio", url: "https://skybound-studio.vercel.app" }],
     robots: {
         index: true,
         follow: true,
@@ -31,14 +31,14 @@ export const metadata: Metadata = {
     openGraph: {
         type: "website",
         locale: "fr_FR",
-        url: "https://skyboundstudio.fr",
+        url: "https://skybound-studio.vercel.app",
         siteName: "Skybound Studio",
         title: "Skybound Studio - Développement Web & Design",
         description:
         "Agence de développement web créative, spécialisée dans les sites modernes, performants et sur mesure.",
         images: [
         {
-            url: "https://skyboundstudio.fr/social.png",
+            url: "https://skybound-studio.vercel.app/social.png",
             width: 1200,
             height: 630,
             alt: "Skybound Studio",
@@ -51,19 +51,21 @@ export const metadata: Metadata = {
         title: "Skybound Studio - Développement Web & Design",
         description:
         "Agence de développement web créative, spécialisée dans les sites modernes, performants et sur mesure.",
-        images: ["https://skyboundstudio.fr/social.png"],
+        images: ["https://skybound-studio.vercel.app/social.png"],
     },
     alternates: {
-        canonical: "https://skyboundstudio.fr",
+        canonical: "https://skybound-studio.vercel.app",
         languages: {
-        "fr-FR": "https://skyboundstudio.fr",
+        "fr-FR": "https://skybound-studio.vercel.app",
         },
     },
 };
+
 export const viewport = {
     width: "device-width",
     initialScale: 1,
 };
+
 export default function RootLayout({
     children,
 }: Readonly<{ children: React.ReactNode }>) {
@@ -82,9 +84,9 @@ export default function RootLayout({
                         __html: JSON.stringify({
                         "@context": "https://schema.org",
                         "@type": "LocalBusiness",
-                        "@id": "https://skyboundstudio.fr#business",
+                        "@id": "https://skybound-studio.vercel.app#business",
                         name: "Skybound Studio",
-                        image: "https://skyboundstudio.fr/social.webp",
+                        image: "https://skybound-studio.vercel.app/social.png",
                         address: {
                             "@type": "PostalAddress",
                             streetAddress: "1 bis Cour d'orbitelle",
@@ -93,7 +95,7 @@ export default function RootLayout({
                             addressCountry: "FR",
                         },
                         telephone: "+33 7 81 07 63 89",
-                        url: "https://skyboundstudio.fr",
+                        url: "https://skybound-studio.vercel.app",
                         }),
                     }}
                 />
@@ -103,8 +105,8 @@ export default function RootLayout({
                         __html: JSON.stringify({
                         "@context": "https://schema.org",
                         "@type": "WebSite",
-                        "@id": "https://skyboundstudio.fr#website",
-                        url: "https://skyboundstudio.fr",
+                        "@id": "https://skybound-studio.vercel.app#website",
+                        url: "https://skybound-studio.vercel.app",
                         name: "Skybound Studio",
                         publisher: {
                             "@type": "Organization",
@@ -116,7 +118,7 @@ export default function RootLayout({
             </head>
             <body className={robotoCondensed.className}>
                 <Header />
-                    <main>{children}</main>
+                <main>{children}</main>
                 <Footer />
                 <Analytics />
                 <SpeedInsights />
