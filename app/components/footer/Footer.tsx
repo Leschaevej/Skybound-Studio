824e5b521dae55f737969bc23390100964bb6a69
Leschaeve Jimmy <leschaeve.jimmy@gmail.com>
2025-09-23 14:11:36 +0200

    0.3.2.3

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,17 +1,21 @@
 'use client';
 
-import "./Footer.scss";
+import './Footer.scss';
 import { robotoSerif } from '../../font';
 import Logo from '../../../app/assets/logo.svg';
 
 export default function Footer() {
-  return (
-    <footer>
-        <Logo className="logo" data-testid="footer-logo" />
-        <p>
-            <span className="copy">© 2025 by </span>
-            <span className={`${robotoSerif.className} brand`}>Skybound Studio</span>
-        </p>
-    </footer>
-  )
+    return (
+        <footer aria-label="Pied de page du site">
+            <Logo
+                className="logo"
+                data-testid="footer-logo"
+                aria-label="Logo Skybound Studio"
+            />
+            <p aria-label="Informations de copyright">
+                <span className="copy">© 2025 by </span>
+                <span className={`${robotoSerif.className} brand`}>Skybound Studio</span>
+            </p>
+        </footer>
+    );
 }
